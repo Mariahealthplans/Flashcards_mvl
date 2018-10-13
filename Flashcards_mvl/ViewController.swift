fc462b5f4f709f228a9ebf6795d5eb2ea735a515
MARIA LOPEZ <MARIALOPEZ@marias-imac.local>
2018-10-13 16:15:34 -0500

    Initial Commit

---
+++ Flashcards_mvl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards_mvl
+//
+//  Created by MARIA LOPEZ on 10/13/18.
+//  Copyright © 2018 MARIA LOPEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
