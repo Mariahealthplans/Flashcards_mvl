6efab65ac7a7014a2ed4fb9a95767907456fd3d7
MARIA LOPEZ <MARIALOPEZ@marias-imac.local>
2018-10-13 19:09:55 -0500

    updated and fixed the iboutlets

--- Flashcards_mvl/ViewController.swift
+++ Flashcards_mvl/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var FrontLabel: UILabel!
+    
+    
+    @IBOutlet weak var BackLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+       FrontLabel.isHidden=true
+        
+    }
 }
-
